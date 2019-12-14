ff19e87416237a5264e11d02cc0cfc04c857d47f
jthk1106 <thk116@gmail.com>
2019-12-14 22:58:12 +0000

    data renders, project functionally complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import './app.css';
+import TopSpot from './topspot';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
+  }
+
   render() {
     return (
-      <div className='App'></div>
+      <div className='App container'>
+        <div className='titleBox row justify-content-center'>
+          <div className='col'>
+            <h2 className='titleBoxContents'>San Diego Top Spots</h2>
+            <p className='titleBoxContents'>A list of the top 30 places to see in San Diego, California.</p>
+          </div>
+        </div>
+        {
+          this.state.topspots.map(topspot => (
+            <TopSpot
+              key={topspot.id}
+              name={topspot.name}
+              description={topspot.description}
+              location={topspot.location}
+            />
+          ))
+        }
+      </div>
     );
   }
 }
---
+++ src/topspot.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default props => {
+  let urlString = `https://maps.google.com/?q=${props.location[0]},${props.location[1]}`
+
+  return (
+    <div className='well'>
+      <h4>{props.name}</h4>
+      <p>{props.description}</p>
+      <p>{props.location[0]}</p>
+      <a href={urlString}>Google Map Link</a>
+    </div>
+  );
+}
